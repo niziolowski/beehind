d201c0523f4509eb702136ee083d708cd203a090
Krystian <krystianpiatkowski92@gmail.com>
2025-08-16 21:16:01 +0200

    fix: more annoying eslint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
-import eslintPluginReact from 'eslint-plugin-react'
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
-  tseslint.configs.recommended,
-  eslintPluginReact.configs.flat.recommended,
-  eslintPluginReact.configs.flat['jsx-runtime'],
+  // DEFAULT
+  // tseslint.configs.recommended,
+  // eslintPluginReact.configs.flat.recommended,
+  // eslintPluginReact.configs.flat['jsx-runtime'],
   {
     settings: {
       react: {
@@ -17,7 +17,20 @@ export default tseslint.config(
     }
   },
   {
+    env: {
+      browser: true,
+      es2022: true,
+      node: true
+    },
+    parser: '@typescript-eslint/parser',
     files: ['**/*.{ts,tsx}'],
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
     plugins: {
       'react-hooks': eslintPluginReactHooks,
       'react-refresh': eslintPluginReactRefresh
