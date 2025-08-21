a822380a2bff2cb13fe12dd429b95bf782fef1b2
Krystian <krystianpiatkowski92@gmail.com>
2025-08-21 22:54:06 +0200

    update: StatusBar

--- src/renderer/src/components/StatusBar.tsx
+++ src/renderer/src/components/StatusBar.tsx
@@ -1,11 +1,19 @@
-import { FiActivity } from 'react-icons/fi'
+import { FiActivity, FiAlertTriangle, FiDownload, FiLoader } from 'react-icons/fi'
 
 const StatusBar = () => {
   return (
-    <div className="flex items-center w-full h-10 flex-none bg-background border-t border-border">
-      <button className="px-5 h-full flex items-center gap-2 text-secondary italic hover:bg-primary">
+    <div className="flex items-center w-full h-10 p-1 flex-none bg-background border-t border-border text-sm">
+      <button className="rounded-xl px-5 h-full flex items-center gap-2 text-secondary italic hover:bg-lime-300 hover:text-font">
+        <FiDownload />
+        <div>Sync</div>
+      </button>
+      <button className="rounded-xl px-5 h-full flex items-center gap-2 text-secondary italic hover:bg-primary">
         <FiActivity />
-        <div>Last synced: </div>
+        <div>synced: 10 min. ago</div>
+      </button>
+      <button className="rounded-xl px-5 h-full flex items-center gap-2 text-red-400 italic hover:bg-red-300 hover:text-font">
+        <FiAlertTriangle />
+        <div>Low stock items: 11 </div>
       </button>
     </div>
   )
